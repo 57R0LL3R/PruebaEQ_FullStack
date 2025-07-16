a769a2963fb4b09ba1fad7c75131875654f5d5d8
57R0LL3R <drcecontre@gmail.com>
2025-07-15 21:50:31 -0500

    Add DBContext and create the first models and fix the private attributes of the models

--- PruebaEQ_API/PruebaEQ_API/Models/DocKey.cs
+++ PruebaEQ_API/PruebaEQ_API/Models/DocKey.cs
@@ -5,12 +5,12 @@ namespace PruebaEQ_API.Models
     public class DocKey
     {
         [Key]
-        int Id { get; set; }
+        public int Id { get; set; }
         [Required]
         [StringLength(200)]
-        string DocName { get; set; }
+        public string DocName { get; set; }
         [Required]
         [StringLength(200)]
-        string Key { get; set; }
+        public string Key { get; set; }
     }
 }
